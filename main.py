3594bb1dafce04df7ff3ee55243e01f6827ffb16
Butachaan <butaokara1202@gmail.com>
2021-02-21 19:31:03 +0900

    修正

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import asyncio
 import random
 import datetime
 import DiscordUtils
+import asyncpg
 from discord.ext import commands
 import utils.json_loader
 updateinfos = "・コマンド追加"
@@ -18,6 +19,36 @@ from discord_slash import SlashCommand, SlashContext
 
 intents = discord.Intents.default()
 intents.members = True
+
+
+async def main():
+    # Establish a connection to an existing database named "test"
+    # as a "postgres" user.
+    conn = await asyncpg.connect(user='pigman', password='Vj27gEKx')
+    # Execute a statement to create a new table.
+    await conn.execute('''
+        CREATE TABLE users(
+            id serial PRIMARY KEY,
+            name text,
+            dob date
+        )
+    ''')
+
+    # Insert a record into the created table.
+    await conn.execute('''
+        INSERT INTO users(name, dob) VALUES($1, $2)
+    ''', 'Bob', datetime.date(1984, 3, 1))
+
+    # Select a row from the table.
+    row = await conn.fetchrow(
+        'SELECT * FROM users WHERE name = $1', 'Bob')
+    # *row* now contains
+    # asyncpg.Record(id=1, name='Bob', dob=datetime.date(1984, 3, 1))
+
+    # Close the connection.
+    await conn.close()
+
+asyncio.get_event_loop().run_until_complete(main())
 
 
 with open('./config.json', 'r') as cjson:

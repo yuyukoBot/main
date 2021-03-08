e23e5d392e7de6d8a10a7329de2b74eed5af163d
Butachaan <butaokara1202@gmail.com>
2021-03-08 14:09:39 +0900

    認証制

--- cog/user_setting.py
+++ cog/user_setting.py
@@ -40,6 +40,8 @@ class Verified(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
 
+    def is_channel(ctx):
+        return ctx.channel.id == 818337568885571624
 
     @commands.Cog.listener()
     async def on_member_join(self,member):
@@ -51,34 +53,33 @@ class Verified(commands.Cog):
                                        name="未認証")
             await member.add_roles(unverified)
 
-def is_channel(ctx):
-    return ctx.channel.id == 818337568885571624
 
-@commands.command()
-@commands.check(is_channel)
-async def verify(self,ctx):
-    unverified = discord.utils.get(ctx.guild.roles, name="未認証")
-    if unverified in ctx.author.roles:
-        msg = await ctx.send('DMに利用規約等載せています')
-        await msg.add_reaction('✅')
-        e = discord.Embed(title="**利用規約**",description="1:Discordの利用規約を守ってください\n2:#メインチャンネル、でのnsfw系のチャットをしないでください\n3:喧嘩など行う場合はDMで\n4:質問などがあれば　#質問で\n5:不具合があれば #不具合報告",color=0x5d00ff)
-        e.add_field(name="認証方法",value="#認証 で`y/agree`を実行してください")
-        await ctx.author.send(embed=e)
 
-    else:
-        await ctx.send('あなたは既に認証されています')
+    @commands.command()
+    @commands.check(is_channel)
+    async def verify(self,ctx):
+        unverified = discord.utils.get(ctx.guild.roles, name="未認証")
+        if unverified in ctx.author.roles:
+            msg = await ctx.send('DMに利用規約等載せています')
+            await msg.add_reaction('✅')
+            e = discord.Embed(title="**利用規約**",description="1:Discordの利用規約を守ってください\n2:#メインチャンネル、でのnsfw系のチャットをしないでください\n3:喧嘩など行う場合はDMで\n4:質問などがあれば　#質問で\n5:不具合があれば #不具合報告",color=0x5d00ff)
+            e.add_field(name="認証方法",value="#認証 で`y/agree`を実行してください")
+            await ctx.author.send(embed=e)
 
-@commands.command()
-@commands.check(is_channel)
-async def agree(self,ctx):
-    unverified = discord.utils.get(ctx.guild.roles, name="未認証")
-    verify = discord.utils.get(ctx.guild.roles, name="Yuyuko’s member")
-    if unverified in ctx.author.roles:  # checks if the user running the command has the unveirifed role
-        # finds the verified role in the guild
-        msg = await ctx.send('認証が完了しました')
-        await msg.add_reaction('✅')
-        await ctx.author.add_roles(verify)
-        await ctx.author.remove_roles(unverified)
+        else:
+            await ctx.send('あなたは既に認証されています')
+
+    @commands.command()
+    @commands.check(is_channel)
+    async def agree(self,ctx):
+        unverified = discord.utils.get(ctx.guild.roles, name="未認証")
+        verify = discord.utils.get(ctx.guild.roles, name="Yuyuko’s member")
+        if unverified in ctx.author.roles:  # checks if the user running the command has the unveirifed role
+
+            msg = await ctx.send('認証が完了しました')
+            await msg.add_reaction('✅')
+            await ctx.author.add_roles(verify)
+            await ctx.author.remove_roles(unverified)
 
 def setup(bot):
     bot.add_cog(Verified(bot))

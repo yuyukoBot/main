971745b9a6984e34bd4103ad45175542fabba189
Butachaan <butaokara1202@gmail.com>
2021-04-10 21:59:55 +0900

    最後の修正

--- util/utils.py
+++ util/utils.py
@@ -16,3 +16,16 @@ def clean_code(content):
         return "\n".join(content.split("\n")[1:])[:-3]
     else:
         return content
+
+
+def getEmbed(ti, desc, color=int("0x61edff", 16), *optiontext):
+    e = discord.Embed(title=ti, description=desc, color=color)
+    nmb = -2
+    while len(optiontext) >= nmb:
+        try:
+            nmb = nmb + 2
+            e.add_field(name=optiontext[nmb], value=optiontext[nmb+1])
+        except IndexError:
+            pass
+    return e
+

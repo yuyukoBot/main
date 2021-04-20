a0999516d5c3cdfd5644dc111b5c43c339b44fa3
Butachaan <butaokara1202@gmail.com>
2021-04-20 20:49:56 +0900

    UPDATE

--- cog/fun.py
+++ cog/fun.py
@@ -31,7 +31,7 @@ import DiscordUtils
 Intents = discord.Intents.default()
 Intents.members = True
 Intents.presences = True
-
+import aiomojang
 
 class fun(commands.Cog, name="Fun"):
 
@@ -50,6 +50,31 @@ class fun(commands.Cog, name="Fun"):
         if hasattr(ctx, 'guild') and ctx.guild is not None:
             await ctx.send(f"Sending you a private message with your random generated password **{ctx.author.name}**")
         await ctx.author.send(f"🎁 **Here is your password:**\n{secrets.token_urlsafe(nbytes)}")
+
+    @commands.command(aliases=["minecraftstats", "statsminecraft", "mcstats"])  # Get information on a player.
+    async def mojang(self, ctx, player: str):
+        profile = aiomojang.Player(player)
+        try:
+            embed = discord.Embed(title=f"<:success:761297849475399710> Information on {player}: ",
+                                  color=ctx.author.color)
+            embed.add_field(name="Player's name: ", value=player)  # Because doing profile.name will raise an error.
+            embed.add_field(name="Player's uuid: ", value=await profile.uuid, inline=False)
+            embed.set_image(url=await profile.get_skin())
+            embed.set_author(name=player, icon_url=await profile.get_skin())
+            embed.set_footer(text="Requested by {}".format(ctx.author.name), icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+        except aiomojang.exceptions.ApiException:
+            return await ctx.send(f"No user with the name {player} was found.")
+
+    @commands.command(aliases=["mchistory"])  # Name history command
+    async def history(self, ctx, player: str):
+        profile = aiomojang.Player(player)
+        embed = discord.Embed(title=f"{player}'s name history: ", color=discord.Colour.blue())
+        i = 1
+        for x in await profile.get_history():
+            embed.add_field(name=f"Name #{i}: ", value=x['name'])  # Iterate through the names.
+            i = i + 1
+        await ctx.send(embed=embed)
 
     @commands.command(aliases=["sl"], description="スロット")
     async def slot(self, ctx):

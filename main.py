5656f6fdeb89bdbb1a704d3ef467e48a4ef3aca0
Butachaan <butaokara1202@gmail.com>
2021-06-06 21:52:55 +0900

    private追加

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from traceback import format_exception
 
 updateinfos = "・コマンド追加"
 import time
-
+import asyncpg
 release = "0.2"
 status = "Beta"
 
@@ -30,7 +30,16 @@ with open('./config.json', 'r') as cjson:
 ver = "2.1"
 token = config["TOKEN"]
 prefix = config["prefix"]
+USERNAME = config["user"]
+PASSWORD = config['password']
+DATABASE = config["database"]
 
+
+async def main():
+    conn = await asyncpg.connect(user=USERNAME,password=PASSWORD,database=DATABASE,host="127.0.0.1")
+    await conn.close()
+
+asyncio.get_event_loop().run_until_complete(main())
 
 bot = commands.Bot(command_prefix=prefix,activety=discord.Game(name="yuyuko"), intents=intents,help_command=None)
 db=sqlite3.connect("main.sqlite",detect_types=sqlite3.PARSE_DECLTYPES, isolation_level=None)

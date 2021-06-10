5488a7b3a4dac0d654732460333bd58231770b3a
Butachaan <butaokara1202@gmail.com>
2021-06-10 18:54:41 +0900

    修正

--- cog/server_setting.py
+++ cog/server_setting.py
@@ -82,6 +82,20 @@ class ServerSetting(commands.Cog):
     async def settings(self, ctx):
         await ctx.send_help(ctx.command)
 
+    @settings.command()
+    async def _reactrole(self, ctx, emoji, role: discord.Role, messageid):
+
+        if ctx.message.author.guild_permissions.manage_messages or ctx.author.id == 478126443168006164:
+            msg = await ctx.channel.fetch_message(messageid)
+            await msg.add_reaction(emoji)
+            db = sqlite3.connect('main.sqlite')
+            cursor = db.cursor()
+            cursor.execute("INSERT INTO reactrole VALUES (?,?,?,?,?)", (role.name, role.id, str(emoji), messageid, ctx.guild.id))
+            db.commit()
+            await ctx.message.delete()
+
+
+
 
 
     @settings.command(description="指定したチャンネルにログを送信します")
@@ -294,105 +308,39 @@ class ServerSetting(commands.Cog):
         else:
             await ctx.send("権限がありません")
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if payload.member.bot:
+            return
+        db = sqlite3.connect('main.sqlite')
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM reactrole WHERE guild_id = ?", (payload.guild_id,))
+        data = cursor.fetchall()
+
+        if not data:
+            return
+        else:
+            for x in data:
+                if x[2] == str(payload.emoji) and int(x[3]) == payload.message_id:
+                    role = discord.utils.get(self.bot.get_guild(payload.guild_id).roles, id=x[1])
+                    await payload.member.add_roles(role)
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self,reaction):
+    async def on_raw_reaction_remove(self, payload):
         db = sqlite3.connect('main.sqlite')
         cursor = db.cursor()
-        if '<' in str(reaction.emoji):
-            cursor.execute(f"SELECT emoji, role, message_id, channel_id FROM reaction WHERE guild_id = '{reaction.guild_id}' and message_id = '{reaction.message_id}' and emoji = '{reaction.emoji.id}'")
-            result = cursor.fetchone()
-            guild = self.bot.get_guild(reaction.guild_id)
-            if result is None:
-                return
-            elif str(reaction.emoji.id) in str(result[0]):
-                on = discord.utils.get(guild.roles, id=int(result[1]))
-                user = guild.get_member(reaction.user_id)
-                await user.add_roles(on)
-            else:
-                return
-        elif '<:' not in str(reaction.emoji):
-            cursor.execute(f"SELECT emoji, role, message_id, channel_id FROM reaction WHERE guild_id = '{reaction.guild_id}' and message_id = '{reaction.message_id}' and emoji = '{reaction.emoji.id}'")
-            result = cursor.fetchone()
-            guild = self.bot.get_guild(reaction.guild_id)
-            if result is None:
-                return
-            elif result is not None:
-                on = discord.utils.get(guild.roles, id=int(result[1]))
-                user = guild.get_member(reaction.user_id)
-                await user.add_roles(on)
-            else:
-                return
+        cursor.execute("SELECT * FROM reactrole WHERE guild_id = ?", (payload.guild_id,))
+        data = cursor.fetchall()
 
-    @commands.Cog.listener()
-    async def on_raw_reaction_remove(self, reaction):
-        db = sqlite3.connect('main.sqlite')
-        cursor = db.cursor()
-        if '<' in str(reaction.emoji):
-            cursor.execute(
-                f"SELECT emoji, role, message_id, channel_id FROM reaction WHERE guild_id = '{reaction.guild_id}' and message_id = '{reaction.message_id}' and emoji = '{reaction.emoji.id}'")
-            result = cursor.fetchone()
-            guild = self.bot.get_guild(reaction.guild_id)
-            if result is None:
-                return
-            elif str(reaction.emoji.id) in str(result[0]):
-                on = discord.utils.get(guild.roles, id=int(result[1]))
-                user = guild.get_member(reaction.user_id)
-                await user.remove_roles(on)
-            else:
-                return
-        elif '<:' not in str(reaction.emoji):
-            cursor.execute(
-                f"SELECT emoji, role, message_id, channel_id FROM reaction WHERE guild_id = '{reaction.guild_id}' and message_id = '{reaction.message_id}' and emoji = '{reaction.emoji.id}'")
-            result = cursor.fetchone()
-            guild = self.bot.get_guild(reaction.guild_id)
-            if result is None:
-                return
-            elif result is not None:
-                on = discord.utils.get(guild.roles, id=int(result[1]))
-                user = guild.get_member(reaction.user_id)
-                await user.remove_roles(on)
-            else:
-                return
+        if not data:
+            return
+        else:
+            for x in data:
+                if str(payload.emoji)[-1:] in x[2] and int(x[3]) == payload.message_id:
+                    role = discord.utils.get(self.bot.get_guild(payload.guild_id).roles, id=x[1])
+                    await self.bot.get_guild(payload.guild_id).get_member(payload.user_id).remove_roles(role)
 
-    @commands.command()
-    async def roleadd(self,ctx,channel:discord.TextChannel,messageid, emoji, role:discord.Role):
-        db = sqlite3.connect('main.sqlite')
-        cursor = db.cursor()
-        cursor.execute(f"SELECT emoji, role, message_id, channel_id FROM reaction WHERE guild_id = '{ctx.message.guild.id}' and message_id = '{messageid}'")
-        result = cursor.fetchone()
-        if '<:' in emoji:
-            emm = re.sub(':.*?:','',emoji).strip('<>')
-            if result is None:
-                sql = ("INSERT INTO reaction(emoji, role, message_id, channel_id, guild_id) VALUES(?,?,?,?,?)")
-                val = (emm, role.id, messageid, channel.id, ctx.guild.id)
-                msg = await channel.fetch_message(messageid)
-                em = self.bot.get_emoji(int(emm))
-                await msg.add_reaction(em)
 
-            elif str(messageid) not in str(result[3]):
-                sql = ("INSERT INTO reaction(emoji, role, message_id, channel_id, guild_id) VALUES(?,?,?,?,?)")
-                VAL = (emm, role.id, messageid, channel.id, ctx.guild.id)
-                msg = await channel.fetch_message(messageid)
-                em = self.bot.get_emoji(int(emm))
-                await msg.add_reaction(em)
-
-        elif '<:' not in emoji:
-            if result is None:
-                sql = ("INSERT INTO reaction(emoji, role, message_id, channel_id, guild_id) VALUES(?,?,?,?,?)")
-                val = (emoji, role.id, messageid, channel.id, ctx.guild.id)
-                msg = await channel.fetch_message(messageid)
-                await msg.add_reaction(emoji)
-
-            elif str(messageid) not in str(result[3]):
-                sql = ("INSERT INTO reaction(emoji, role, message_id, channel_id, guild_id) VALUES(?,?,?,?,?)")
-                VAL = (emoji, role.id, messageid, channel.id, ctx.guild.id)
-                msg = await channel.fetch_message(messageid)
-                await msg.add_reaction(emoji)
-        cursor.execute(sql,val)
-        db.commit()
-        cursor.close()
-        db.close()
 
 
 def setup(bot):

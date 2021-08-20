ea5452a383daa3722c6691a5d4326eec52b5b889
Butachaan <butaokara1202@gmail.com>
2021-08-21 02:05:00 +0900

    修正

--- cog/yuyuko_help.py
+++ cog/yuyuko_help.py
@@ -1,299 +1,151 @@
+import itertools
+
 import discord
-from discord.ext import commands, menus
-import asyncio
+from discord.ext import commands
+from util import page
 
 
-# Taken From https://github.com/Rapptz/RoboDanny
-
-
-class RoboPages(menus.MenuPages):
-    def __init__(self, source):
-        super().__init__(source=source, check_embeds=True)
-
-        self.input_lock = asyncio.Lock()
-
-    async def finalize(self, timed_out):
-        try:
-            if timed_out:
-                await self.message.clear_reactions()
-            else:
-                await self.message.delete()
-        except discord.HTTPException:
-            pass
-
-
-class BotHelpPageSource(menus.ListPageSource):
-    def __init__(self, help_command, commands):
-        # entries = [(cog, len(sub)) for cog, sub in commands.items()]
-        # entries.sort(key=lambda t: (t[0].qualified_name, t[1]), reverse=True)
-        super().__init__(
-            entries=sorted(commands.keys(), key=lambda c: c.qualified_name), per_page=6
-        )
-        self.commands = commands
-        self.help_command = help_command
-        self.prefix = help_command.clean_prefix
-
-    @staticmethod
-    def format_commands(cog, commands):
-        # A field can only have 1024 characters so we need to paginate a bit
-        # just in case it doesn't fit perfectly
-        # However, we have 6 per page so I'll try cutting it off at around 800 instead
-        # Since there's a 6000 character limit overall in the embed
-        if cog.description:
-            short_doc = cog.description.split("\n", 1)[0] + "\n"
-        else:
-            short_doc = "No help found...\n"
-
-        current_count = len(short_doc)
-        ending_note = "+%d not shown"
-        ending_length = len(ending_note)
-
-        page = []
-        for command in commands:
-            value = f"`{command.name}`"
-            count = len(value) + 1  # The space
-            if count + current_count < 800:
-                current_count += count
-                page.append(value)
-            else:
-                # If we're maxed out then see if we can add the ending note
-                if current_count + ending_length + 1 > 800:
-                    # If we are, pop out the last element to make room
-                    page.pop()
-
-                # Done paginating so just exit
-                break
-
-        if len(page) == len(commands):
-            # We're not hiding anything so just return it as-is
-            return short_doc + " ".join(page)
-
-        hidden = len(commands) - len(page)
-        return short_doc + " ".join(page) + "\n" + (ending_note % hidden)
-
-    async def format_page(self, menu, cogs):
-        description = (
-            'y/help <コマンド名で確認できます'
-        )
-
-        embed = discord.Embed(
-            title="Categories", description=description, color=0x5d00ff
-        )
-
-        for cog in cogs:
-            commands = self.commands.get(cog)
-            if commands:
-                value = self.format_commands(cog, commands)
-                embed.add_field(name=cog.qualified_name, value=value)
-
-        maximum = self.get_max_pages()
-        embed.set_footer(text=f"Page {menu.current_page + 1}/{maximum}")
-        return embed
-
-
-class GroupHelpPageSource(menus.ListPageSource):
-    def __init__(self, group, commands, *, prefix):
-        super().__init__(entries=commands, per_page=6)
-        self.group = group
-        self.prefix = prefix
-        self.title = f"{self.group.qualified_name} Commands"
-        self.description = self.group.description
-
-    async def format_page(self, menu, commands):
-        embed = discord.Embed(
-            title=self.title,
-            description=self.description,
-            color=0x5d00ff,
-        )
-
-        for command in commands:
-            signature = f"{command.qualified_name} {command.signature}"
-            embed.add_field(
-                name=signature,
-                value=command.description or "No help given...",
-                inline=True,
-            )
-
-        maximum = self.get_max_pages()
-        if maximum > 1:
-            embed.set_author(
-                name=f"Page {menu.current_page + 1}/{maximum} ({len(self.entries)} commands)"
-            )
-
-        embed.set_footer(
-            text=f'Use "{self.prefix}help command" for more info on a command.'
-        )
-        return embed
-
-
-class HelpMenu(RoboPages):
-    def __init__(self, source):
-        super().__init__(source)
-
-    @menus.button("\N{WHITE QUESTION MARK ORNAMENT}", position=menus.Last(5))
-    async def show_bot_help(self, payload):
-        """Shows how to use the bot."""
-        embed = discord.Embed(color=0x5d00ff)
-        embed.title = "How Interpret The Help Pages"
-
-        entries = (
-            ('\N{BLACK LEFT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}\ufe0f', "一番前のページに戻る"),
-            ('\N{BLACK LEFT-POINTING TRIANGLE}\ufe0f', "前のページに戻る"),
-            ('\N{BLACK RIGHT-POINTING TRIANGLE}\ufe0f', "次のページに行く"),
-            ('\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}\ufe0f',"最後のページに行く"),
-            ('\N{BLACK SQUARE FOR STOP}\ufe0f','ページをstopする'),
-            ('\N{INPUT SYMBOL FOR NUMBERS}','指定したページに飛ぶ'),
-
-        )
-
-        for name, value in entries:
-            embed.add_field(name=name, value=value, inline=True)
-
-        embed.set_footer(
-            text=f"We were on page {self.current_page + 1} before this message."
-        )
-        await self.message.edit(embed=embed)
-
-        async def go_back_to_current_page():
-            await asyncio.sleep(30.0)
-            try:
-                await self.show_page(self.current_page)
-            except discord.errors.NotFound:
-                return
-
-        self.bot.loop.create_task(go_back_to_current_page())
-
-    @menus.button('\N{INPUT SYMBOL FOR NUMBERS}', position=menus.Last(1.5), lock=False)
-    async def numbered_page(self, payload):
-        """lets you type a page number to go to"""
-        if self.input_lock.locked():
-            return
-
-        async with self.input_lock:
-            channel = self.message.channel
-            author_id = payload.user_id
-            to_delete = []
-            to_delete.append(await channel.send('What page do you want to go to?'))
-
-            def message_check(m):
-                return m.author.id == author_id and \
-                       channel == m.channel and \
-                       m.content.isdigit()
-
-            try:
-                msg = await self.bot.wait_for('message', check=message_check, timeout=30.0)
-            except asyncio.TimeoutError:
-                to_delete.append(await channel.send('Took too long.'))
-                await asyncio.sleep(5)
-            else:
-                page = int(msg.content)
-                to_delete.append(msg)
-                await self.show_checked_page(page - 1)
-
-            try:
-                await channel.delete_messages(to_delete)
-            except Exception:
-                pass
-
-
-class PaginatedHelpCommand(commands.HelpCommand):
+class CustomHelpCommand(commands.HelpCommand):
     def __init__(self):
         super().__init__(
-            command_attrs={
-                "cooldown": commands.Cooldown(1, 3.0, commands.BucketType.member),
-                "help": "Shows help about the bot, a command, or a category",
-            }
+            command_attrs={"help": "Show help about the bot, a command, or a category."}
         )
 
     async def on_help_command_error(self, ctx, error):
         if isinstance(error, commands.CommandInvokeError):
             await ctx.send(str(error.original))
 
-    @staticmethod
-    def get_command_signature(command):
-        parent = command.full_parent_name
-        if len(command.aliases) > 0:
-            aliases = "|".join(command.aliases)
-            fmt = f"[{command.name}|{aliases}]"
-            if parent:
-                fmt = f"{parent} {fmt}"
-            alias = fmt
-        else:
-            alias = command.name if not parent else f"{parent} {command.name}"
-        return f"{alias} {command.signature}"
+    def make_page_embed(self, commands, title="Help", description=discord.Embed.Empty):
+        embed = discord.Embed(color=discord.Color.blurple())
+        embed.title = title
+        embed.description = description
+        embed.set_footer(
+            text=f'Use "{self.context.clean_prefix}help command" for more info on a command.'
+        )
+
+        for command in commands:
+            signature = self.context.clean_prefix + command.qualified_name + " " + command.signature
+            help = command.help or "No help found..."
+
+            embed.add_field(
+                name=signature,
+                value=help.splitlines()[0],
+                inline=False,
+            )
+
+        return embed
+
+    def make_default_embed(self, cogs, title="Command Categories", description=discord.Embed.Empty):
+        embed = discord.Embed(color=discord.Color.blurple())
+        embed.title = title
+        embed.description = description
+
+        counter = 0
+        for cog in cogs:
+            cog, description, command_list = cog
+            description = f"{description or 'No Description'} \n {''.join([f'`{command.qualified_name}` ' for command in command_list])}"
+            embed.add_field(name=cog.qualified_name, value=description, inline=False)
+            counter += 1
+
+        return embed
 
     async def send_bot_help(self, mapping):
-        bot = self.context.bot
-        entries = await self.filter_commands(bot.commands, sort=True)
+        ctx = self.context
+        bot = ctx.bot
 
-        all_commands = {}
-        for command in entries:
-            if command.cog is None:
+        def get_category(command):
+            cog = command.cog
+            return cog.qualified_name if cog is not None else "\u200bNo Category"
+
+        pages = []
+        total = 0
+
+        filtered = await self.filter_commands(bot.commands, sort=True, key=get_category)
+
+        for cog_name, commands in itertools.groupby(filtered, key=get_category):
+            commands = sorted(commands, key=lambda c: c.name)
+
+            if len(commands) == 0:
                 continue
-            try:
-                all_commands[command.cog].append(command)
-            except KeyError:
-                all_commands[command.cog] = [command]
 
-        menu = HelpMenu(BotHelpPageSource(self, all_commands))
-        await menu.start(self.context)
+            total += len(commands)
+            cog = bot.get_cog(cog_name)
+            description = (
+                (cog and cog.description)
+                if (cog and cog.description) is not None
+                else discord.Embed.Empty
+            )
+            pages.append((cog, description, commands))
+
+        async def get_page(pidx):
+            cogs = pages[min(len(pages) - 1, pidx * 6) : min(len(pages) - 1, pidx * 6 + 6)]
+
+            embed = self.make_default_embed(
+                cogs,
+                title=f"Command Categories (Page {pidx+1}/{len(pages)//6+1})",
+                description=(
+                    f"Use `{self.context.clean_prefix}help <command>` for more info on a command.\n"
+                    f"Use `{self.context.clean_prefix}help <category>` for more info on a category."
+                ),
+            )
+
+            # embed.set_author(name=f"Page {pidx + 1}/{len(pages)} ({total} commands)")
+
+            return embed
+
+        paginator = page.Paginator(get_page, len(pages) // 6 + 1)
+        await paginator.send(ctx)
 
     async def send_cog_help(self, cog):
-        entries = await self.filter_commands(cog.get_commands(), sort=True)
-        menu = HelpMenu(GroupHelpPageSource(cog, entries, prefix=self.clean_prefix))
-        await menu.start(self.context)
+        ctx = self.context
 
-    def common_command_formatting(self, embed_like, command):
-        embed_like.title = f"{self.context.prefix}{self.get_command_signature(command)}"
-        if command.description:
-            embed_like.description = f"```{command.description}\n\n{command.help}```"
-        else:
-            embed_like.description = f"```{command.help}```" or "```No help found...```"
+        filtered = await self.filter_commands(cog.get_commands(), sort=True)
 
-    async def send_command_help(self, command):
-        params = " ".join(command.clean_params.keys())
-        embed = discord.Embed(title=f"{self.context.prefix}{command.qualified_name} {params}",
-                              description=command.description, color=0xb300ff)
-        if command.aliases:
-            embed.add_field(name="有効なエイリアス：", value="`" + "`, `".join(command.aliases) + "`", inline=False)
-        else:
-            embed.add_field(name="有効なエイリアス", value="ありません")
-        if command.help:
-            embed.add_field(name="ユーザーに必要な権限", value=f"`{command.help}`", inline=False)
-        if command.brief:
-            embed.add_field(name="Botに必要な権限",value=f"`{command.brief}`", inline=False)
-        if command.usage:
-            embed.add_field(name="使い方",value=f"`{command.usage}`",inline=False)
-        await self.get_destination().send(embed=embed)
+        embed = self.make_page_embed(
+            filtered,
+            title=(cog and cog.qualified_name or "Other") + " Commands",
+            description=discord.Embed.Empty if cog is None else cog.description,
+        )
+
+        await ctx.send(embed=embed)
 
     async def send_group_help(self, group):
+        ctx = self.context
+
         subcommands = group.commands
         if len(subcommands) == 0:
             return await self.send_command_help(group)
 
-        entries = await self.filter_commands(subcommands, sort=True)
-        if len(entries) == 0:
-            return await self.send_command_help(group)
+        filtered = await self.filter_commands(subcommands, sort=True)
 
-        source = GroupHelpPageSource(group, entries, prefix=self.clean_prefix)
-        self.common_command_formatting(source, group)
-        menu = HelpMenu(source)
-        await menu.start(self.context)
+        embed = self.make_page_embed(
+            filtered,
+            title=group.qualified_name,
+            description=f"{group.description}\n\n{group.help}"
+            if group.description
+            else group.help or "No help found...",
+        )
+
+        await ctx.send(embed=embed)
+
+    async def send_command_help(self, command):
+        embed = discord.Embed(color=discord.Color.blurple())
+        embed.title = self.context.clean_prefix + command.qualified_name
+
+        if command.description:
+            embed.description = f"{command.description}\n\n{command.help}"
+        else:
+            embed.description = command.help or "No help found..."
+
+        embed.add_field(name="Signature", value=self.get_command_signature(command))
+
+        await self.context.send(embed=embed)
 
 
-class _help(commands.Cog, name="help"):
-    """For the help command."""
-
-    def __init__(self, bot: commands.Bot) -> None:
-        self.bot = bot
-        self.old_help_command = bot.help_command
-        bot.help_command = PaginatedHelpCommand()
-        bot.help_command.cog = self
-
-    def cog_unload(self):
-        self.bot.help_command = self.old_help_command
+def setup(bot):
+    bot.old_help_command = bot.help_command
+    bot.help_command = CustomHelpCommand()
 
 
-def setup(bot: commands.Bot) -> None:
-    """Starts help cog."""
-    bot.add_cog(_help(bot))
+def teardown(bot):
+    bot.help_command = bot.old_help_command
---
+++ util/language.py
@@ -0,0 +1,30 @@
+import os
+import json
+import sqlite3
+
+def get_lan(id, text):
+    default_language = "ja"
+
+    if os.path.exists("main.db"):
+        conn = sqlite3.connect("main.db", isolation_level=None)
+        c = conn.cursor()
+        c.execute("SELECT * FROM userdata WHERE id=:Id", {"Id": id})
+        temp = c.fetchone()
+        if temp is None:
+            language = default_language
+        else:
+            language = temp[1]
+            if not os.path.exists(f"lang/{language}.json"):
+                language = default_language
+        conn.close()
+        # read language file
+        with open(f"lang/{language}.json", 'rt', encoding='UTF8') as f:
+            language_data = json.load(f)
+        return language_data[text]
+
+    else:
+        with open(f"lang/{default_language}.json", 'rt', encoding='UTF8') as f:
+            language_data = json.load(f)
+        return language_data[text]
+
+
